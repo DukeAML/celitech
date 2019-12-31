0e0cb6957baf6e59b9c342111913bd796ab16a0a
Mary Gooneratne <mmg53@duke.edu>
2019-12-31 00:20:42 -0800

    Added combined data and script

---
+++ csv_combiner.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import pandas as pd
+
+def read_and_parse(f):
+    #get rid of first row and concatenate
+    df = pd.read_csv(f)
+    df = df.iloc[1:]
+    return df
+
+os.chdir("sample_data")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#combine all files in the list
+sample_data = pd.concat([read_and_parse(f) for f in all_filenames ])
+#export to csv
+sample_data.to_csv( "sample_data.csv")
+
