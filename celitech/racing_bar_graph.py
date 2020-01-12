8c2279893cc74891fe6a3454def49e24306cb1ce
LUKE TRUITT <lot@duke.edu>
2020-01-12 00:27:39 -0500

    updated animator

--- celitech/racing_bar_graph.py
+++ celitech/racing_bar_graph.py
@@ -18,14 +18,18 @@ countries = {}
 for country in pycountry.countries:
     countries[country.name] = country.alpha_3
 
-# Generate dictionary of country vs. usage for particular day
-def country_by_day(month, day):
+# Generate dictionary of country vs. usage up to particular day
+def country_by_day(day):
+    month = int(day / 30) + 7
+    day = day % 30 + 1
     date = "2019-"+str(month).zfill(2)+"-"+str(day).zfill(2)
 
-    usage_dict = {}
+    usage_dict = initialize_countries_usage()
+    
     for index, row in df.iterrows():
         row_date = row["CLOSE_TIME"]
-        if date in row_date:
+        row_day, row_month = parse_date(row_date)
+        if (int(row_day)<=int(day) and int(row_month)==int(month)) or (int(row_month)<int(month)):
             country_name = str(row["COUNTRY_NAME"])
             country_code =  countries.get(country_name)
             duration = int(row["DURATION"])
@@ -34,8 +38,19 @@ def country_by_day(month, day):
                 usage_dict[country_code] = usage_dict[country_code] + duration
             else:
                 usage_dict[country_code] = duration
-    return sorted(usage_dict)
+    return usage_dict
 
+def parse_date(date):
+    d = date.split('-')
+    day = d[2].split('T')[0]
+    month = d[1]
+    return day, month
+
+def initialize_countries_usage():
+    countries = {}
+    for country in countries_in_data:
+        countries[country] = 0
+    return countries
 # Generate usage vs country dictionary for a particular month
 def data_for_month(month):
     date = "2019-"+str(month).zfill(2)
@@ -80,12 +95,10 @@ def get_month(time_string):
     return month
 
 
-
+print()
 fig, ax = plt.subplots(figsize=(15, 8))
-def draw_barchart(month):
+def month_barchart(month):
     month_dict = data_for_month(month)
-    print(month_dict)
-    print(month_dict)
     ax.clear()
     y_pos = np.arange(len(month_dict.keys()))
     ax.barh(y_pos, month_dict.values(), align='center')
@@ -95,7 +108,18 @@ def draw_barchart(month):
     ax.set_xlabel('Data Usage (GB)')
     ax.set_title('Data Usage by Country')
 
-animator = FuncAnimation(fig, draw_barchart, frames=range(7,10), interval=1000)
-# HTML(animator.to_jshtml()) 
+def day_barchart(day):
+    day_dict = country_by_day(day)
+    ax.clear()
+    y_pos = np.arange(len(day_dict.keys()))
+    ax.barh(y_pos, day_dict.values(), align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(day_dict.keys())
+    ax.invert_yaxis()
+    ax.text(1, 0.4, day, transform=ax.transAxes, color='#777777', size=46, ha='right', weight=800)
+    ax.set_xlabel('Data Usage (GB)')
+    ax.set_title('Data Usage by Country')
 
+animator = FuncAnimation(fig, day_barchart, frames=range(0,60), interval=1000)
+# HTML(animator.to_jshtml()) 
 plt.show()
